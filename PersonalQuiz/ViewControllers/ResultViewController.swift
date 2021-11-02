eda752ada6d1a850c26908a85e1fb41ea6877661
Alexey Parfenov <aiparfenov88@gmail.com>
2021-11-02 20:39:20 +0300

    HW done

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -21,27 +21,21 @@ class ResultViewController: UIViewController {
     }
     
     private func updateResult() {
-        var frequencyOfAnimals: [AnimalType: Int] = [:]
+        var countOfAnimals: [AnimalType: Int] = [:]
         let animals = responses.map { $0.type }
         
         for animal in animals {
-            frequencyOfAnimals[animal] = (frequencyOfAnimals[animal] ?? 0) + 1
+            countOfAnimals[animal] = (countOfAnimals[animal] ?? 0) + 1
         }
         
-        let sortedFrequencyOfAnimals = frequencyOfAnimals.sorted { $0.value > $1.value }
-        guard let mostFrequensyAnimal = sortedFrequencyOfAnimals.first?.key else { return }
+        let sortedFrequencyOfAnimals = countOfAnimals.sorted { $0.value > $1.value }
+        guard let mostCountedAnimal = sortedFrequencyOfAnimals.first?.key else { return }
         
-        updateUI(with: mostFrequensyAnimal)
+        updateUI(with: mostCountedAnimal)
     }
     
     private func updateUI(with animal: AnimalType) {
         resultAnswerLabel.text = "Вы - \(animal.rawValue)"
         resultDefinitionLabel.text = "\(animal.definition)"
     }
-    
-    // 1. Передать сюда массив с ответами
-    // 2. Определить наиболее часто встречающийся тип животного
-    // 3. Отобразить результаты в соотвствии с этим животным
-    // 4. Избавиться от кнопки возврата назад на экране результатов
-    
 }
